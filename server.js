38ed7609f39ce21fdaf5f7d07f9d64dcc9f0fd70
ashwini27 <ashwinikh027@gmail.com>
2026-04-02 15:45:28 +0530

    add user APIs for create and fetch users

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 connectDB();
 
 // Middleware to parse JSON
 app.use(express.json());
+app.use("/api/users", userRoutes);
 
 // Test route
 app.get("/", (req, res) => {
